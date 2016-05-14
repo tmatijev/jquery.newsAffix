1f1653ea1c4d864fae802048936d1bd4c0078c77
tmatijev <matas0412@gmail.com>
2016-05-14 17:22:09 +0200

    Initial commit

---
+++ js/jquery.tm-news-affix.js
@@ -0,0 +1,249 @@
+;(function ( $, window, document, undefined ) {
+    
+    // Create the defaults once
+    var pluginName  = 'newsAffix';
+    var defaults    = {
+        headerWrapper: "#targeted-wrapper",
+        headerClass: ".content__title",
+        sidebar: ".js-news-affix-items",
+        newsAffixSel: ".js-news-affix",
+        setAffixClasses: {
+            lower: {
+                cssClass: '.news-affix__bar-inverse',
+                cssRule: 'height'
+            },
+            higher: {
+                cssClass: '.news-affix__bar',
+                cssRule: 'width'
+            }
+        },
+        breakPoint: 1250
+    };
+    
+    function Plugin( element, options ) {
+        this.element        = element;
+        this.options        = $.extend( {}, defaults, options) ;
+        this._defaults      = defaults;
+        this._name          = pluginName;
+        this.headers        = {};
+        this._sidebarIndex  = 0;
+        this._currentPos    = 0;
+        this._newsAffixPos  = 0;
+        this.isAffixActive  = false;
+        this.$affix         = $(this.options.newsAffixSel);
+        this._activeChapter = 0;
+        this._$sidebar      = $(this.options.sidebar);
+        this._affixStyle    = '';
+        
+        this.init();
+    }
+
+    Plugin.prototype.init = function () {
+        this.setNewsAffixPosition();
+        this.setHeaderPosition();
+        this.setActiveHeader();
+        this.setScrollHandler();
+        this.toggleNewsAffix();
+        this.onResizeEvent();
+        this.checkResponsiveState();
+    };
+    
+    Plugin.prototype.setScrollHandler = function () {
+        var scrollTimeout;
+        var that = this;
+        
+        $(window).scroll(function () {
+            if (scrollTimeout) {
+                clearTimeout(scrollTimeout);
+                scrollTimeout = null;
+            }
+            
+            scrollTimeout = setTimeout(that.scrollHandler(), 20);
+        });
+    };
+    
+    Plugin.prototype.helperDebounce = function (func, wait, immediate) {
+        var timeout;
+        return function() {
+            var context = this;
+            var args    = arguments;
+            var later   = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            
+            var callNow = immediate && !timeout;
+            
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            
+            if (callNow) {
+                func.apply(context, args);
+            }
+        };
+    };
+    
+    Plugin.prototype.onResizeEvent = function () {
+        var that = this;
+        
+        var myEfficientFn = this.helperDebounce(function() {
+            var resolution = that.getCurrentResolution();
+            that.checkResponsiveState(resolution);
+        }, 250);
+
+        window.addEventListener('resize', myEfficientFn);
+    };
+    
+    Plugin.prototype.checkResponsiveState = function (resolution) {
+        resolution = resolution || this.getCurrentResolution();
+        affixClass = resolution < this.options.breakPoint ? ["lower", "higher"] : ["higher", "lower"];
+        
+        this.setAffixClass(affixClass);
+        this.setPercentageOfChapter();
+    };
+    
+    Plugin.prototype.setAffixClass = function (affixClass) {
+        if( this.$affix.hasClass(affixClass[0]) ) {
+            return false;
+        }
+        
+        this._affixStyle = affixClass[0];
+        
+        this.$affix
+            .removeClass(affixClass[1])
+                .addClass(affixClass[0]);
+    };
+    
+    Plugin.prototype.scrollHandler = function () {
+        this.toggleNewsAffix();
+        this.setActiveHeader();
+        this.setPercentageOfChapter();
+    };
+    
+    Plugin.prototype.toggleNewsAffix = function () {
+        /**
+         * @TODO: Return false if is active but
+         * else must exist to hide sidebar again
+         */
+        
+        if( this.getScrollTopOnLoad() > this.headers[1].start ) {
+            this.$affix.addClass('active');
+            // this.isAffixActive = true;
+        } else {
+            // console.log("yea");
+            this.$affix.removeClass('active');
+            // this.isAffixActive = false;
+        }
+    };
+    
+    Plugin.prototype.setHeaderPosition = function () {
+        var $wrapper = $(this.options.headerWrapper);
+        var naItems = $wrapper.find('[data-na-header]');
+        
+        /**
+         * @TODO: Better logic
+         */
+        
+        for( var i = 0; i < naItems.length; i++ ) {
+            this.headers[i + 1] = {
+                start: $(naItems[i]).offset().top
+            };
+            
+            if( i + 1 === 6 ) {
+                var chapterHeight = $(naItems[i]).parent().height();
+                var chapterEnd = parseInt($(window).height() - chapterHeight, 10);
+                var final = chapterHeight + $(naItems[i]).offset().top;
+                
+                this.headers[i + 1].end = final;
+            }
+            
+            if( i + 1 > 1 ) {
+                this.headers[i]['end'] = $(naItems[i]).offset().top - 1;
+            }
+        }
+    };
+    
+    Plugin.prototype.setActiveHeader = function () {
+        var length = Object.keys(this.headers).length;
+        var currentPosition = this.getScrollTopOnLoad();
+        
+        for( var key in this.headers ) {
+            if( this.headers.hasOwnProperty(key) ) {
+                var isCurrentHeader = this.getCurrentPosition({
+                    start: this.headers[key].start,
+                    end: this.headers[key].end,
+                    current: currentPosition,
+                    index: Number(key)
+                });
+                
+                if( isCurrentHeader ) {
+                    this.setSidebarActiveClass(Number(key));
+                    break;
+                }
+            }
+        }
+    };
+    
+    Plugin.prototype.setPercentageOfChapter = function (param) {
+        
+        if( !this._sidebarIndex ) {
+            return false;
+        }
+        
+        var step        = parseInt(this.headers[this._sidebarIndex].end - this.headers[this._sidebarIndex].start, 10);
+        var current     = parseInt(this.getScrollTopOnLoad() - this.headers[this._sidebarIndex].start, 10);
+        var percentage  = parseFloat(current / step * 100).toFixed(2) + '%';
+        var affixClass  = this.options.setAffixClasses[this._affixStyle].cssClass;
+        var affixRule   = this.options.setAffixClasses[this._affixStyle].cssRule;
+        
+        this._$sidebar
+            .find('*[data-na-side="'+Number(this._sidebarIndex)+'"]')
+            .find(affixClass)
+                .css(affixRule, percentage);
+    };
+    
+    Plugin.prototype.setSidebarActiveClass = function (index) {
+        if( this._sidebarIndex !== index ) {
+            this._sidebarIndex  = index;
+            
+            this._$sidebar.find('[data-na-side]').removeClass('active');
+            this._$sidebar.find('*[data-na-side="' + Number(index) + '"]').addClass('active');
+            
+            this.setSidebarCompletedChapters(this._sidebarIndex);
+        }
+    };
+    
+    Plugin.prototype.setSidebarCompletedChapters = function (currentIndex) {
+        this._$sidebar.find('.news-affix__item').removeClass('completed');
+        
+        for( var i = 0; i < currentIndex - 1; i++ ) {
+            this._$sidebar.find('.news-affix__item').eq(i).addClass('completed');
+        }
+    };
+    
+    Plugin.prototype.setNewsAffixPosition = function () {
+        this._newsAffixPos = $(this.options.newsAffixSel).offset().top;
+    };
+    
+    Plugin.prototype.getCurrentResolution = function () {
+        return window.innerWidth || document.documentElement.clientWidth;
+    };
+    
+    Plugin.prototype.getCurrentPosition = function (param) {
+        return param.current > param.start && param.current <= param.end;
+    };
+    
+    Plugin.prototype.getScrollTopOnLoad = function () {
+        return $('body').scrollTop();
+    };
+    
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, 
+                    new Plugin( this, options ));
+            }
+        });
+    };
+
+})( jQuery, window, document );
